1d486d4dc904b92632bba7212a5ae1284473be40
user8 <acer>
2018-10-09 21:03:47 +0800

    7-87. CLIENT: create-react-app, then proxy to 3002 server, also concurrently run both server

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  componentDidMount() {
+    axios.get("/api/product/brands").then(res => {
+      console.log(res);
+    });
+  }
+
+  render() {
+    return <div className="App">My APP</div>;
+  }
+}
+
+export default App;
