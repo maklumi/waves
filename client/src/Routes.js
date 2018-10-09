74a0364c8af2aec7202b8895104299ec862ac3a9
user8 <acer>
2018-10-10 07:03:07 +0800

    7-89. CLIENT: What you need to setup redux

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import "./Resources/css/styles.css";
 import Layout from "./hoc/Layout";
 import Home from "./components/Home";
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "./Routes";
+import "./Resources/css/styles.css";
+
 import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import Reducers from "./store/reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const App = (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 );
 ReactDOM.render(App, document.getElementById("root"));
