e461228f826371279f9796e49dc20de6e2a3afe8
user8 <acer>
2018-10-10 21:18:13 +0800

    8-100. CLIENT: Creating Header links and logoutUser action

--- client/src/store/actions/user_actions.js
+++ client/src/store/actions/user_actions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER, REGISTER_USER, AUTH_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from "./types";
 import { USER_SERVER } from "../../components/utils/misc";
 
 export function registerUser(dataToSubmit) {
@@ -24,13 +24,24 @@ export function loginUser(dataToSubmit) {
   };
 }
 
-export function auth(dataToSubmit) {
+export function auth() {
   const request = axios
-    .get(`${USER_SERVER}/auth`, dataToSubmit)
+    .get(`${USER_SERVER}/auth`)
     .then(response => response.data);
 
   return {
     type: AUTH_USER,
+    payload: request
+  };
+}
+
+export function logoutUser() {
+  const request = axios
+    .get(`${USER_SERVER}/logout`)
+    .then(response => response.data);
+
+  return {
+    type: LOGOUT_USER,
     payload: request
   };
 }
