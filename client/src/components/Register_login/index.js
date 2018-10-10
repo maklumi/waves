2b40cf72a85c1b5bea326ccde0765687a022464a
user8 <acer>
2018-10-10 09:34:27 +0800

    7-91. Creating the login -1, custom FormField using state props

--- client/src/components/Register_login/index.js
+++ client/src/components/Register_login/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MyButton from "../utils/Button";
+import Login from "./Login";
 
 const RegisterLogin = () => {
   return (
@@ -20,6 +21,7 @@ const RegisterLogin = () => {
       <div className="right">
         <h2>Registered Customer</h2>
         <p>If you have an account log in.</p>
+        <Login />
       </div>
     </div>
   );
