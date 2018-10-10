5e46122f4aa40df2d6e1fd7797488a6dcd9b4a7a
user8 <acer>
2018-10-10 12:46:16 +0800

    7-95. CLIENT: Register new user UI and standard state

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,11 +4,13 @@ import { Switch, Route } from "react-router-dom";
 import Layout from "./hoc/Layout";
 import Home from "./components/Home";
 import RegisterLogin from "./components/Register_login";
+import Register from "./components/Register_login/Register";
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/register" exact component={Register} />
         <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/" exact component={Home} />
       </Switch>
