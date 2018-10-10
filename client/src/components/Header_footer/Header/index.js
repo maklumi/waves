a8bf023cf66018eb394efd07e0bba9ec29f374ee
user8 <acer>
2018-10-10 20:52:57 +0800

    7-99. CLIENT: Header links UI

--- client/src/components/Header_footer/Header/index.js
+++ client/src/components/Header_footer/Header/index.js
@@ -1,19 +1,95 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <header className="bck_b_light">
-      <div className="container">
-        <div className="left">
-          <div className="logo">WAVES</div>
-        </div>
-        <div className="right">
-          <div className="top">LINKS</div>
-          <div className="bottom">LINKS</div>
-        </div>
-      </div>
-    </header>
+import { connect } from "react-redux";
+
+class Header extends Component {
+  state = {
+    page: [
+      {
+        name: "Home",
+        linkTo: "/",
+        public: true
+      },
+      {
+        name: "Guitars",
+        linkTo: "/shop",
+        public: true
+      }
+    ],
+    user: [
+      {
+        name: "My Cart",
+        linkTo: "/user/cart",
+        public: false
+      },
+      {
+        name: "My Account",
+        linkTo: "/user/dashboard",
+        public: false
+      },
+      {
+        name: "Login",
+        linkTo: "/register_login",
+        public: true
+      },
+      {
+        name: "Log out",
+        linkTo: "/user/logout",
+        public: false
+      }
+    ]
+  };
+
+  defaultLink = (item, i) => (
+    <Link to={item.linkTo} key={i}>
+      {item.name}
+    </Link>
   );
-};
 
-export default Header;
+  showLinks = type => {
+    let list = [];
+
+    if (this.props.user.userData) {
+      type.forEach(item => {
+        if (!this.props.user.userData.isAuth) {
+          if (item.public === true) {
+            list.push(item);
+          }
+        } else {
+          if (item.name !== "Login") {
+            list.push(item);
+          }
+        }
+      });
+    }
+
+    return list.map((item, i) => {
+      return this.defaultLink(item, i);
+    });
+  };
+
+  render() {
+    return (
+      <header className="bck_b_light">
+        <div className="container">
+          <div className="left">
+            <div className="logo">WAVES</div>
+          </div>
+          <div className="right">
+            <div className="top">{this.showLinks(this.state.user)}</div>
+            <div className="bottom">{this.showLinks(this.state.page)}</div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps)(Header);
