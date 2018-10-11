42630d63fcd4f620bdb4dc0b2bb1dadab38635a2
user8 <acer>
2018-10-11 15:04:29 +0800

    8-108. CLIENT: Radio groups for price list and filters array holds that value

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -3,9 +3,10 @@ import PageTop from "../utils/PageTop";
 
 import { connect } from "react-redux";
 import { getBrands, getWoods } from "../../store/actions/product_actions";
-import { frets } from "../utils/Form/fixed_categories";
+import { frets, price } from "../utils/Form/fixed_categories";
 
 import CollapseCheckBox from "../utils/CollapseCheckBox";
+import CollapseRadio from "../utils/CollapseRadio";
 
 class Shop extends Component {
   state = {
@@ -25,9 +26,25 @@ class Shop extends Component {
     this.props.dispatch(getWoods());
   };
 
+  handlePrice = value => {
+    const data = price;
+    let array = [];
+    for (let key in data) {
+      if (data[key]._id === parseInt(value, 10)) {
+        array = data[key].array;
+      }
+    }
+    return array;
+  };
+
   handleFilters = (filters, category) => {
     const newFilters = { ...this.state.filters };
     newFilters[category] = filters;
+
+    if (category === "price") {
+      let priceValue = this.handlePrice(filters);
+      newFilters[category] = priceValue;
+    }
 
     this.setState({ filters: newFilters });
   };
@@ -57,6 +74,12 @@ class Shop extends Component {
                 title="Woods"
                 list={products.woods}
                 handleFilters={filters => this.handleFilters(filters, "woods")}
+              />
+              <CollapseRadio
+                initState={true}
+                title="Price"
+                list={price}
+                handleFilters={filters => this.handleFilters(filters, "price")}
               />
             </div>
             <div className="right">right</div>
--- client/src/components/utils/Form/fixed_categories.js
+++ client/src/components/utils/Form/fixed_categories.js
@@ -16,3 +16,36 @@ export const frets = [
     name: 26
   }
 ];
+
+export const price = [
+  {
+    _id: 0,
+    name: "Any",
+    array: []
+  },
+  {
+    _id: 1,
+    name: "RM0 to RM300",
+    array: [0, 300]
+  },
+  {
+    _id: 2,
+    name: "RM301 to RM600",
+    array: [301, 600]
+  },
+  {
+    _id: 3,
+    name: "RM601 to RM1000",
+    array: [601, 1000]
+  },
+  {
+    _id: 4,
+    name: "RM1001 to RM3000",
+    array: [1001, 3000]
+  },
+  {
+    _id: 5,
+    name: " above RM3000",
+    array: [3000, 1000000000]
+  }
+];
