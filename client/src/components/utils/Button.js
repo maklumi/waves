76595db47395f6c86ac0c0facc07f5b847d59f22
user8 <acer>
2018-10-11 08:52:37 +0800

    8-104. CLIENT: Card has View product button and shopping icon with altClass

--- client/src/components/utils/Button.js
+++ client/src/components/utils/Button.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import { faShoppingBag } from "@fortawesome/fontawesome-free-solid";
+
 const Button = props => {
   const buttons = () => {
     let template = "";
     switch (props.type) {
       case "default":
         template = (
-          <Link className="link_default" to={props.linkTo} {...props.addStyle}>
+          <Link
+            className={!props.altClass ? "link_default" : props.altClass}
+            to={props.linkTo}
+            {...props.addStyle}
+          >
             {props.title}
           </Link>
+        );
+        break;
+      case "bag_link":
+        template = (
+          <div className="bag_link" onClick={props.runAction}>
+            <FontAwesomeIcon icon={faShoppingBag} />
+          </div>
         );
         break;
       default:
--- client/src/components/utils/Card.js
+++ client/src/components/utils/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import MyButton from "./Button";
 
 class Card extends Component {
   renderCardImage(images) {
@@ -19,7 +20,7 @@ class Card extends Component {
             background: `url(${this.renderCardImage(props.images)}) no-repeat`
           }}
         />
-        <div className="actions">
+        <div className="action_container">
           <div className="tags">
             <div className="brand">{props.brand.name}</div>
             <div className="name">{props.name}</div>
@@ -27,6 +28,25 @@ class Card extends Component {
           </div>
         </div>
         {props.grid ? <div className="description">description</div> : null}
+        <div className="actions">
+          <div className="button_wrapp">
+            <MyButton
+              type="default"
+              altClass="card_link"
+              title="View product"
+              linkTo={`/product_detail/${props._id}`}
+              addStyles={{
+                margin: "10px 0 0 0"
+              }}
+            />
+          </div>
+          <div className="button_wrapp">
+            <MyButton
+              type="bag_link"
+              runAction={() => console.log("added to cart")}
+            />
+          </div>
+        </div>
       </div>
     );
   }
