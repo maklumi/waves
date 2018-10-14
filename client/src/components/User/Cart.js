3f4a0eb12362067f68883bbfddbf47b461cfcff0
user8 <acer>
2018-10-14 21:26:24 +0800

    9-132. ADMIN: Adding total price

--- client/src/components/User/Cart.js
+++ client/src/components/User/Cart.js
@@ -29,13 +29,36 @@ class Cart extends Component {
         this.props
           .dispatch(getCartItems(cartItem, user.userData.cart))
           .then(a => {
-            console.log(a);
+            if (this.props.user.cartDetail.length > 0) {
+              this.calculateTotal(this.props.user.cartDetail);
+            }
           });
       }
     }
   };
 
+  calculateTotal = cartDetail => {
+    let total = 0;
+
+    cartDetail.forEach(item => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
+
+    this.setState({
+      total,
+      showTotal: true
+    });
+  };
+
   removeFromCart = id => {};
+
+  showNoItemMessage = () => (
+    <div className="cart_no_items">
+      <FontAwesomeIcon icon={faFrown} />
+      <div>You have no item</div>
+    </div>
+  );
+
   render() {
     return (
       <UserLayout>
@@ -47,7 +70,25 @@ class Cart extends Component {
               type="cart"
               removeItem={id => this.removeFromCart(id)}
             />
+            {this.state.showTotal ? (
+              <div>
+                <div className="user_cart_sum">
+                  <div>Total amount: RM {this.state.total}</div>
+                </div>
+              </div>
+            ) : this.state.showSuccess ? (
+              <div className="cart_success">
+                <FontAwesomeIcon icon={faSmile} />
+                <div>Thank you</div>
+                <div>Your order is now complete</div>
+              </div>
+            ) : (
+              this.showNoItemMessage()
+            )}
           </div>
+          {this.state.showTotal ? (
+            <div className="paypal_button_container">Paypal</div>
+          ) : null}
         </div>
       </UserLayout>
     );
